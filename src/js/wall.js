193edcc251eff538142f22523831820ce534be80
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-07-31 02:12:16 -0500

    corrigiendo funcionalidad en comentarios editar-eliminar

--- src/js/wall.js
+++ src/js/wall.js
@@ -6,73 +6,77 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-function compartir(){
 
-	console.log("entra el boton");
-	//let mensajes = document.getElementById("comentario").value;
-	
-	db.collection("users").add({// agregando id
-    	first: mensajes
-    
-	})// si se agrega correctamente 
-	.then(function(docRef) {
-	    console.log("Document written with ID: ", docRef.id);
-	})// si se agrega erroneo
-	.catch(function(error) {
-	    console.error("Error adding document: ", error);
-	});
-}
-
-//pintando los datos
- 
- let pintar = document.getElementById("printComments").value;
- db.collection("users").onSnapshot((querySnapshot) =>{
- 	printComments.innerHTML="";
- 	querySnapshot.forEach((doc)=>{
- 		console.log(`${doc.id} => ${doc.data().first}`);
- 		printComments.innerHTML+=`
-        <section>
-        <div>${doc.data().first}</div>
-        <!--Por cada boton que se crea contiene los datos-->
-        <button class="btn btn-danger" onclick="eliminar('${doc.id}')">Elimar</button>
-        <button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().first}')">Editar</button>
-        </section>
-        `
- 	});
- });
-
- const eliminar = (id) => {
-    db.collection("users").doc(id).delete().then(function() {
-        console.log("Document successfully deleted!");
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-    });
-}
-
-const editar = (id,comentario) =>{
-
-    document.getElementById("comentario").value = comentario ;
-    //el boton con este id"guardar" se modificara su texto html a editar
-    let boton = document.getElementById("botonCompartir");
-    boton.innerHTML = "Editar";
-    //Al presionar el boton q dice editar c correra la sig funcion
-    boton.onclick() = function(){ 
-        var washingtonRef = db.collection("users").doc(id);
-        // Set the "capital" field of the city 'DC'
-    var comentario = document.getElementById("comentario").value;
-    return washingtonRef.update({
-        first: comentario,
-       
-    })
-    .then(function() {
-        console.log("Document successfully updated!");
-        boton.innerHTML = "Guardar";
-    })
-    .catch(function(error) {
-        // The document probably doesn't exist.
-        console.error("Error updating document: ", error);
-    });
+function publicar() {
+    let mensajes = document.getElementById("comentario").value;
+  
+    if ( mensajes !== "" ) {
+      db.collection("users").add({
+        mensaje: mensajes,
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        document.getElementById("comentario").value = "";
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+    } else {
+      alert('ingresa tu mensaje')
     }
-         
-}
+  }
+  
+  let publicacion = document.getElementById("publicacion");
+  db.collection("users").onSnapshot((querySnapshot) => {
+    publicacion.innerHTML= "";
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().mensaje}`);
+        //para usuario va <th scope="row">${doc.id}</th>
+        //probar iinsertAdjacentHTML(beforebegin, texto) para que ordene pùblicaciones
+        publicacion.innerHTML += `
+        <article class="post">
+            <p>${doc.data().mensaje}</p>
+            <a class="post-eliminar" onclick="eliminar('${doc.id}')">Eliminar</a>
+            <a class="post-editar" onclick="editar('${doc.id}','${doc.data().mensaje}')">Editar</a>
+        </article>
+        `
+    });
+  });
+  
+  //Borrar datos
+  function eliminar(id) {
+    db.collection("users").doc(id).delete().then(function() {
+      console.log("Document successfully deleted!");
+    }).catch(function(error) {
+      console.error("Error removing document: ", error);
+    });
+  
+  }
+  
+  //Editar documentos
+  function editar(id,mensaje) {
+    document.getElementById("comentario").value = mensaje;
+    let boton = document.getElementById('boton');
+    boton.innerHTML = "Editar";
+    boton.onclick = function (){
+      const washingtonRef = db.collection("users").doc(id);
+      let mensaje = document.getElementById('comentario').value;
+  
+      return washingtonRef.update({
+        mensaje: mensaje,
+      })
+      .then(function() {
+          console.log("Document successfully updated!");
+          boton.innerHTML = "Guardar";
+          document.getElementById("comentario").value = "";
+      })
+      .catch(function(error) {
+          console.error("Error updating document: ", error);
+      });
+  
+    }
+  
+    }
+  
+
 
