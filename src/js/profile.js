3c01584286855db9f8ae635126ab31acc791fcf8
marfloresrayon <psico.luzy@gmail.com>
2018-07-31 17:11:21 -0500

    Agregando funcionalidad de todos los login a login.js

--- src/js/profile.js
+++ src/js/profile.js
@@ -9,13 +9,31 @@ $(document).ready(function () {
 //Termina NavBar de Materialize 
 
 //Manejo de DOM para cerrar sesión
-// let logOut = document.getElementById('logout')
+let logOut = document.getElementById('logout')
 
-// //Cerrar sesión
-// logOut.addEventListener('click', function () {
-//   firebase.auth().signOut().then(function () {
-//     console.log("Sesión cerrada")
-//   }).catch(function (error) {
-//     console.log(error)
-//   });
-// })
+//Cerrar sesión
+logOut.addEventListener('click', function () {
+  firebase.auth().signOut().then(function () {
+    console.log("Sesión cerrada")
+  }).catch(function (error) {
+    console.log(error)
+  });
+})
+
+//Observador de estado de autenticación
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    user = document.getElementById(user);
+    // User is signed in.
+    const displayName = user.displayName;
+    const email = user.email;
+    const emailVerified = user.emailVerified;
+    const photoURL = user.photoURL;
+    const isAnonymous = user.isAnonymous;
+    const uid = user.uid;
+    const providerData = user.providerData;
+  } else {
+    // User is signed out.
+    console.log('La usuaria no ha iniciado sesión')
+  }
+});
